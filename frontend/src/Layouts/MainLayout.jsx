4daff6a3f3c5ca73b9efff151bf7980b9b62e7f3
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-09-04 00:42:20 +0530

    fix: Refactor loading state management in MainLayout component

--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { NavBar } from "../components/NavBar/NavBar";
 import api from "../api/api";
 import { useUserContext } from "../Context/context";
+import { useState } from "react";
+import { Button } from "../components/ui/button";
 
 export const MainLayout = () => {
-	const { setUser } = useUserContext();
+	const { setUser, user } = useUserContext();
 	const location = useLocation();
 
 	// Paths where NavBar should NOT be shown
 	const hideNavBarOn = ["/anonymous-call"];
 
 	const shouldHideNavBar = hideNavBarOn.includes(location.pathname);
-
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		async function fetchUserProfile() {
 			try {
@@ -20,15 +22,26 @@ export const MainLayout = () => {
 				setUser(data);
 			} catch (error) {
 				console.error("Error fetching user profile:", error);
+			} finally {
+				setLoading(false);
 			}
 		}
 		fetchUserProfile();
 	}, [setUser]);
-
-	return (
+	return loading ? (
+		<div className="flex items-center justify-center min-h-svh bg-black text-white">
+			<div className="animate-spin rounded-full h-16 w-16 border-b-2 border-white"></div>
+		</div>
+	) : user ? (
 		<div>
 			{!shouldHideNavBar && <NavBar />}
 			<Outlet />
+		</div>
+	) : (
+		<div className="min-h-svh flex items-center justify-center bg-black text-white">
+			<Button variant="outline" asChild>
+				<Link to="/login">Login</Link>
+			</Button>
 		</div>
 	);
 };
